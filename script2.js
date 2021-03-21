f34721a0ca770b1dab89d89018c236e3db571582
Deven <masterdeven@gmail.com>
2021-03-21 02:04:49 -0400

    debug script2.js

--- script2.js
+++ script2.js
@@ -5,7 +5,7 @@ function getRandomInt (min, max) {
 fetch('quotes.json')
     .then(response => response.json())
     .then(data => {
-        // console.log(data.motivation.length)
+        console.log(data.motivation.length)
         var index = getRandomInt(0,data.motivation.length)
         console.log(data.motivation[index])
         document.querySelector("#quote-text").innerHTML = data.motivation[index].quote;
